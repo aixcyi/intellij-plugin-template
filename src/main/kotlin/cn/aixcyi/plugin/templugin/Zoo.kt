0a6d39c84d8eb4a3edb59f4928be2e63b4fe05d5
砹小翼 <75880483+aixcyi@users.noreply.github.com>
2024-08-05 12:10:17 +0800

    i18n: 默认语言改为英语，抽离国际化工具，改用 DynamicBundle

--- src/main/kotlin/cn/aixcyi/plugin/templugin/Zoo.kt
+++ src/main/kotlin/cn/aixcyi/plugin/templugin/Zoo.kt
@@ -1,8 +1,5 @@
 package cn.aixcyi.plugin.templugin
 
-import org.jetbrains.annotations.PropertyKey
-import java.util.*
-
 @Suppress("unused")
 object Zoo {
     /**
@@ -11,27 +8,18 @@ object Zoo {
     const val PLUGIN_STORAGE = "HooTool.xml"
 
     /**
-     * 配置所对应的 XML 的组件名。
+     * 国际化文本包路径。
+     *
+     * 对应 `./src/kotlin/resources/messages/PluginTemplate.properties`
+     */
+    const val BUNDLE_NAME = "messages.PluginTemplate"
+
+    /**
+     * 插件配置所对应的 XML 的组件名。
      *
      * `<component name="NAME"></component>`
      */
-    object XmlComponent
-
-    /** 文本资源包。 */
-    private val BUNDLE = ResourceBundle.getBundle("messages.PluginTemplate")
-
-    /**
-     * 获取本地化翻译。
-     *
-     * Retrieve translated text via keys on "properties" file.
-     *
-     * @param key properties 文件中的键。
-     * @return properties 文件中键对应的值。
-     */
-    @JvmStatic
-    fun message(@PropertyKey(resourceBundle = "messages.PluginTemplate") key: String): String {
-        return BUNDLE.getString(key)
-            .replace("\\r\\n", "\n")  // "\r\n" -> LF
-            .replace("\\n", "\n")  // "\n" -> LF
+    object XmlComponent {
+        // const val OPTIONS = "MyOptions"
     }
 }
